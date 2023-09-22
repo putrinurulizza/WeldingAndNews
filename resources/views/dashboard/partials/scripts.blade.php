b872f45d2b9c580b67d9e4d2295e2f028c9a8648
TajulMunandar <tajulmunandar701@gmail.com>
2023-09-22 18:25:26 +0700

    text editor

--- resources/views/dashboard/partials/scripts.blade.php
+++ resources/views/dashboard/partials/scripts.blade.php
@@ -1,6 +1,8 @@
   <!-- build:js assets/vendor/js/core.js -->
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+  </script>
   <script src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
 
   <script src="{{ asset('vendor/js/menu.js') }}"></script>
